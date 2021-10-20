6c375f7af62b356b77b07f87e3af3f96ae0c7ac3
Lal krishna N C <lalukrishnavallil01@gmail.com>
2021-10-20 20:00:39 +0530

    front end designing with routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import { BrowserRouter,Route } from 'react-router-dom';
 import './App.css';
+import Navbar from './components.js/NavBar';
+import Signin from './components.js/views/signin';
+import Signup from './components.js/views/signup';
+import Home from './components.js/views/home';
+import Profile from './components.js/views/profile';
 
 function App() {
   return (
-   <h1>hello</h1>
+  <BrowserRouter>
+   <Navbar/>
+   <Route exact path="/">
+     <Home/>
+  </Route>
+  <Route path="/signup">
+    <Signup/>
+    </Route>
+    <Route path="/signin">
+      <Signin/>
+      </Route>
+      <Route path="/profile">
+        <Profile/>
+        </Route>
+  </BrowserRouter>
   );
 }
 
---
+++ src/components.js/NavBar.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import M from "materialize-css"
+const Navbar = () =>{
+ return(
+   
+
+    <div className="navbar-fixed">
+
+    <nav>
+        <div className="front_view">
+            <div className="nav-wrapper green">
+            <Link to="/" className="brand-logo center">HOME</Link>
+            <Link to="/" data-target="sidenav-list" className="sidenav-trigger right"><i className="material-icons">menu</i></Link>
+            <ul >
+            <li><Link to ="/signup">signup</Link> </li>
+            <li><Link to ="/signin">signin</Link> </li>
+            <li><Link to ="/profile">profile</Link></li>
+        </ul>
+       
+   </div>
+   </div>
+ </nav>
+ <ul id="sidenav-list" class="sidenav">
+ <li><Link to="/"><i class="material-icons">cloud</i>First Link With Icon</Link></li>
+    <li><a href="#!">Second Link</a></li>
+    <li><div class="divider"></div></li>
+    <li><a class="subheader">Subheader</a></li>
+    <li><a class="waves-effect" href="#!">Third Link With Waves</a></li>
+  </ul>
+  
+
+
+<h1>{document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(elems, {});
+  })};
+</h1>
+
+
+ </div>
+ 
+
+ 
+     
+ );
+}
+
+export default Navbar;
